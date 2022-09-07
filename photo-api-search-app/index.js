3bd03a84f7c0ee7f2088eec4781bfae721168fe8
Ben Kilkowski <benkilkowski@outlook.com>
2022-09-06 20:51:11 -0500

    Dynamic search with query string manipulation based on search bar value

--- photo-api-search-app/index.js
+++ photo-api-search-app/index.js
@@ -1,13 +1,22 @@
 const apiKey = "563492ad6f917000010000016e91732b17cf466a822cb5f55bf38b06";
-const pexelsUrl = "https://api.pexels.com/v1/search";
 
-$.ajax({
-    type: "GET",
-    url: pexelsUrl,
-    beforeSend: function(xhr) {
-        xhr.setRequestHeader("Authorization", apiKey);
-    },
-    success: function(data) {
-        console.log(data);
-    }
+$("#search-form").submit(function(e) {
+    e.preventDefault();
+
+    let query = $("#search-bar").val();
+
+    $.ajax({
+        method: "GET",
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("Authorization", apiKey);
+        },
+        url: "https://api.pexels.com/v1/search?query=" + query + "&per_page=15&page=1",
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error)
+        {
+            console.log(error);
+        }
+    });
 });
